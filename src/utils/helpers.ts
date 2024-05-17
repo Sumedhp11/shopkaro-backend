0c2b92993dacb9d033b2bbe84987feca49d50f57
Sumedh Pawar <sumedhpawar8966@gmail.com>
2024-05-17 15:02:19 +0530

    changed the helper function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,17 +1,20 @@
-import { CookieOptions, Response } from "express";
+import { CookieOptions, Request, Response } from "express";
 import { UserInterface } from "../interfaces/UserInterface.js";
 import jwt from "jsonwebtoken";
-const cookieOption: CookieOptions = {
-  httpOnly: true,
-  secure: true,
-};
 
 const sendToken = (
+  req: Request,
   res: Response,
   user: UserInterface,
   statusCode: number,
   message: string
 ) => {
+  const origin = req.get("Origin");
+  const isLocalhost = origin?.includes("localhost");
+  const cookieOption: CookieOptions = {
+    httpOnly: true,
+    secure: !isLocalhost,
+  };
   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET!);
   return res
     .status(statusCode)
