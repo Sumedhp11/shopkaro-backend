73c8d2eb2be69fffd6fcc07c0ccdc02050aa16d7
Sumedh Pawar <sumedhpawar8966@gmail.com>
2024-05-18 01:15:54 +0530

    changed the api structure of getCartById

--- src/controllers/cart-controller.ts
+++ src/controllers/cart-controller.ts
@@ -85,8 +85,7 @@ const getCartbyUserId = async (
     return res.status(200).json({
       success: true,
       message: "Cart Retrieved Successfully",
-      data: savedCart,
-      totalAmount,
+      data: { savedCart, totalAmount },
     });
   } catch (error) {
     console.log(error);
